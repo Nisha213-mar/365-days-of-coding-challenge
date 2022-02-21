3364ac3629cd8ef652d20034765c30bdad272c45
Nisha Jha <83752180+Nisha213-mar@users.noreply.github.com>
2022-02-21 19:49:36 +0530

    Datatypes in javaScript

---
+++ day60-90/day73/datatypes.js
@@ -0,0 +1,46 @@
+//Primitive Data Types
+
+//string
+let name="Nisha";
+console.log("my name is "+ name);
+console.log("Data type is " +(typeof name));
+
+//Numbers
+let marks=34;
+console.log("Data type is " +(typeof marks));
+
+//Boolean
+let isDriver=true;
+console.log("Data type is " +(typeof isDriver));
+
+//Null
+let nullVar=null;
+console.log("Data type is " +(typeof nullVar));
+
+//Undefined
+let undef=undefined;
+console.log("Data type is " +(typeof undef));
+
+//Reference Data types
+
+//Arrays
+
+myarr=[1,2,3,4,false,"string"];
+console.log("Data type is "+(typeof myarr));
+
+//Object Literals
+let stMarks={
+    harry:90,
+    Subham:67,
+    Rohan:56
+}
+console.log(typeof stMarks);
+
+function findName(){
+
+}
+console.log(typeof findName);
+
+let date=new Date();
+console.log(typeof date);
+
