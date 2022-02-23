fc8169416f0f927b813860001c5963abae9cce73
Nisha Jha <83752180+Nisha213-mar@users.noreply.github.com>
2022-02-23 19:26:37 +0530

    Windows in js

---
+++ day60-90/day75/window.js
@@ -0,0 +1,16 @@
+console.log("This is about window in JavaScript");
+let a=window.document;
+//alert('hello Nisha');
+a=prompt('This will destroy your computer . Type your name');
+//a=confirm('Are you sure you want to delete this page');
+
+a=window.innerHeight;
+a=window.innerWidth;
+console.log(a);
+b=scrollX;
+b=location;
+console.log(b);
+c=location.toString();
+console.log(c);
+d=window.history;
+console.log(d);
