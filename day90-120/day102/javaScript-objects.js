d1a00c8616af9e7f1f98eae74a9bf8084cc5fbe5
Nisha Jha <83752180+Nisha213-mar@users.noreply.github.com>
2022-03-22 20:10:47 +0530

    Objects in js Exercise

---
+++ day90-120/day102/javaScript-objects.js
@@ -0,0 +1,189 @@
+console.log('This is about JavaScript Objects Exercise');
+
+const myJson = `{
+    "word": "example",
+    "results": [
+      {
+        "definition": "a representative form or pattern",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "model"
+        ],
+        "typeOf": [
+          "representation",
+          "internal representation",
+          "mental representation"
+        ],
+        "hasTypes": [
+          "prefiguration",
+          "archetype",
+          "epitome",
+          "guide",
+          "holotype",
+          "image",
+          "loadstar",
+          "lodestar",
+          "microcosm",
+          "original",
+          "paradigm",
+          "pilot",
+          "prototype",
+          "template",
+          "templet",
+          "type specimen"
+        ],
+        "derivation": [
+          "exemplify"
+        ],
+        "examples": [
+          "I profited from his example"
+        ]
+      },
+      {
+        "definition": "something to be imitated",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "exemplar",
+          "good example",
+          "model"
+        ],
+        "typeOf": [
+          "ideal"
+        ],
+        "hasTypes": [
+          "pacemaker",
+          "pattern",
+          "beauty",
+          "prodigy",
+          "beaut",
+          "pacesetter"
+        ],
+        "derivation": [
+          "exemplify",
+          "exemplary"
+        ]
+      },
+      {
+        "definition": "an occurrence of something",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "case",
+          "instance"
+        ],
+        "typeOf": [
+          "happening",
+          "natural event",
+          "occurrence",
+          "occurrent"
+        ],
+        "hasTypes": [
+          "clip",
+          "mortification",
+          "piece",
+          "time",
+          "humiliation",
+          "bit"
+        ],
+        "derivation": [
+          "exemplify"
+        ],
+        "examples": [
+          "but there is always the famous example of the Smiths"
+        ]
+      },
+      {
+        "definition": "an item of information that is typical of a class or group",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "illustration",
+          "instance",
+          "representative"
+        ],
+        "typeOf": [
+          "information"
+        ],
+        "hasTypes": [
+          "excuse",
+          "apology",
+          "specimen",
+          "case in point",
+          "sample",
+          "exception",
+          "quintessence",
+          "precedent"
+        ],
+        "derivation": [
+          "exemplify",
+          "exemplary"
+        ],
+        "examples": [
+          "this patient provides a typical example of the syndrome",
+          "there is an example on page 10"
+        ]
+      },
+      {
+        "definition": "punishment intended as a warning to others",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "deterrent example",
+          "lesson",
+          "object lesson"
+        ],
+        "typeOf": [
+          "monition",
+          "admonition",
+          "word of advice",
+          "warning"
+        ],
+        "derivation": [
+          "exemplary"
+        ],
+        "examples": [
+          "they decided to make an example of him"
+        ]
+      },
+      {
+        "definition": "a task performed or problem solved in order to develop skill or understanding",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "exercise"
+        ],
+        "typeOf": [
+          "lesson"
+        ],
+        "examples": [
+          "you must work the examples at the end of each chapter in the textbook"
+        ]
+      }
+    ],
+    "syllables": {
+      "count": 3,
+      "list": [
+        "ex",
+        "am",
+        "ple"
+      ]
+    },
+    "pronunciation": {
+      "all": "ɪɡ'zæmpəl"
+    },
+    "frequency": 4.67
+  }`;
+
+  const myObj = JSON.parse(myJson);
+  console.log('The object is :',myObj);
+  console.log('The results in the object are :',myObj['results']);
+
+  let meanings=document.getElementById('meanings')
+  meanings.addEventListener('click',()=>{
+      console.log('Someone clicked meanings');
+      populate();
+  })
+
+  function populate(){
+      let results=myObj['results'];
+      let html="";
+      results.forEach(element => {
+          html+='<li>One of the definition of example is ${element} </li>'
+      });
+  }
